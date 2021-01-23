f1ffee4261b80cafcc7ca4abdd9b28e0e4e3340a
Cabe Branson <1258606+bitmap@users.noreply.github.com>
2021-01-23 13:59:32 -0500

    Setup TypeScript

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,29 @@
+import typescript from '@rollup/plugin-typescript'
+
 export default [
   // ES Module
   {
-    input: 'lib/index.js',
+    input: 'lib/index.ts',
     output: {
       file: 'dist/index.js',
       format: 'es',
     },
+    plugins: [typescript({
+      moduleResolution: 'node',
+      target: 'es6',
+    })],
   },
 
   // Common.js
   {
-    input: 'lib/index.js',
+    input: 'lib/index.ts',
     output: {
       file: 'dist/index.cjs',
       format: 'cjs',
     },
+    plugins: [typescript({
+      moduleResolution: 'classic',
+      target: 'es5',
+    })],
   },
 ]
