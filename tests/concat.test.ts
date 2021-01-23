9d350bf1906266377c6b2cf1e7ffeab9520eecab
Cabe Branson <1258606+bitmap@users.noreply.github.com>
2021-01-23 14:27:08 -0500

    Update concat to TypeScript

--- tests/concat.test.ts
+++ tests/concat.test.ts
@@ -3,7 +3,7 @@ import { concat, curry } from '../lib'
 describe('concat', () => {
   const arr1 = [1, 2, 3, 4]
   const arr2 = [4, 5, 6, 7]
-  const arr3 = [7, 8, 9, 0]
+  const arr3 = ['7', '8', 9, 0]
 
   const curryConcat = curry(a => b => concat(a, b))
   const partial = curryConcat(arr1)
@@ -11,7 +11,7 @@ describe('concat', () => {
   test('concats arrays', () => {
     expect(concat(arr1)).toStrictEqual([1, 2, 3, 4])
     expect(concat(arr1, arr2)).toStrictEqual([1, 2, 3, 4, 4, 5, 6, 7])
-    expect(concat(arr1, arr2, arr3)).toStrictEqual([1, 2, 3, 4, 4, 5, 6, 7, 7, 8, 9, 0])
+    expect(concat(arr1, arr2, arr3)).toStrictEqual([1, 2, 3, 4, 4, 5, 6, 7, '7', '8', 9, 0])
   })
 
   test('concats partials', () => {
