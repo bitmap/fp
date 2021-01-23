7d4f537874cc28f1b8fef5475f6078dbf29f7123
Cabe Branson <1258606+bitmap@users.noreply.github.com>
2021-01-23 14:34:46 -0500

    Update pipe/compose to TypeScript

---
+++ lib/compose.ts
@@ -0,0 +1,10 @@
+import { flow } from './utils/flow'
+
+/**
+ * Compose unary function output from right to left.
+ *
+ * `compose(a, b, c)(x)` is equivelent to `a(b(c(x)))`
+ *
+ * `compose :: [(a -> b)] -> a -> b`
+ */
+export const compose = flow(true)
---
+++ lib/utils/flow.ts
@@ -0,0 +1,17 @@
+import { fold } from './fold'
+
+type Fn = (arg: any) => any
+type ArgT<T> = T extends (arg: infer A) => any ? A : never;
+
+/**
+ * Utility to scaffold compose/pipe. Accepts a list of unary functions as
+ * arguments. Returns a reducer based on direction.
+ *
+ * `flow :: boolean -> [(a -> b)] -> a -> b`
+ */
+export const flow = (fromRight: boolean) =>
+  <Fns extends Fn[]>(...fns: Fns) =>
+    (value: ArgT<Fns[0]>): ArgT<Fns[0]> =>
+      fold(fromRight)(
+        (memo: Fn, fn: Fn) => fn(memo), value, fns,
+      )
