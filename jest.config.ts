8c05623e3e6799065a6be10d9d71dee02e244ebc
Cabe Branson <1258606+bitmap@users.noreply.github.com>
2021-01-23 13:59:32 -0500

    Setup TypeScript

---
+++ jest.config.ts
@@ -0,0 +1,11 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest',
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testPathIgnorePatterns: [
+    'tmp',
+  ],
+}
