d00f5a0d51146ffba217bd5cb575343d8161dad7
Cabe Branson <1258606+bitmap@users.noreply.github.com>
2021-01-27 14:48:06 -0500

    Fix docs formatting

--- lib/all.ts
+++ lib/all.ts
@@ -3,7 +3,7 @@ import { curry } from './curry'
 /**
  * Returns true if all items in list meet the condition. `all` args are curried.
  *
- * all :: (a -> boolean) -> [a] -> boolean
+ * `all :: (a -> boolean) -> [a] -> boolean`
  */
 export const all = curry(
   <T>(predicate: (value: T) => boolean, list: T[]): boolean => list.every(predicate),
