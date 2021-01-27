d338d6bda3af4fc42e2f1b886ec1d7803aea06a3
Cabe Branson <1258606+bitmap@users.noreply.github.com>
2021-01-27 14:46:05 -0500

    Fix types for find and findLast

--- lib/find.ts
+++ lib/find.ts
@@ -4,8 +4,8 @@ import { curry } from './curry'
  * Returns first item in a list that meets the condition. If no item meets the
  * criteria, it returns undefined. `find` args are curried.
  *
- * find :: (a -> boolean) -> [a] -> a | undefined
+ * `find :: (a -> boolean) -> [a] -> a | undefined`
  */
 export const find = curry(
-  <T>(predicate: (value: T) => unknown, list: T[]): T => list.find(predicate),
+  <T>(predicate: (value: T) => boolean, list: T[]): T | undefined => list.find(predicate),
 )
--- lib/findLast.ts
+++ lib/findLast.ts
@@ -5,8 +5,8 @@ import { reverse } from './reverse'
  * Returns last item in a list that meets the condition. If no item meets the
  * criteria, it returns undefined. `findLast` args are curried.
  *
- * findLast :: (a -> boolean) -> [a] -> a | undefined
+ * `findLast :: (a -> boolean) -> [a] -> a | undefined`
  */
 export const findLast = curry(
-  <T>(predicate: (value: T) => unknown, list: T[]): T => reverse(list).find(predicate),
+  <T>(predicate: (value: T) => boolean, list: T[]): T | undefined => reverse(list).find(predicate),
 )
