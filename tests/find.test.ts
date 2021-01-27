72997c33286838fe24fb4a600061cadd441b5d0a
Cabe Branson <1258606+bitmap@users.noreply.github.com>
2021-01-27 10:57:12 -0500

    Convert find to TypeScript

---
+++ tests/find.test.ts
@@ -0,0 +1,9 @@
+import { find } from '..'
+
+test('find', () => {
+  const findGreaterThanTen = find(x => x > 10)
+
+  expect(findGreaterThanTen([2, 0, 4, -9, 10, 1])).toBeUndefined()
+  expect(findGreaterThanTen([0, 5, 20, 1, 5, 25])).toStrictEqual(20)
+  expect(findGreaterThanTen([138, 45, 16, 79, 1])).toStrictEqual(138)
+})
