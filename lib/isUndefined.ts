f197265c2e5d4b2860f8c89088289950542d1420
Cabe Branson <1258606+bitmap@users.noreply.github.com>
2021-01-28 15:33:10 -0500

    Use isTypeOf to make isUndefined util

--- lib/isUndefined.ts
+++ lib/isUndefined.ts
@@ -1,6 +1,8 @@
+import { isTypeOf } from './isTypeOf'
+
 /**
  * Returns true if value is undefined
  *
  * `isUndefined :: a -> boolean`
  */
-export const isUndefined = (data: unknown): data is undefined => typeof data === 'undefined'
+export const isUndefined = isTypeOf('undefined')
