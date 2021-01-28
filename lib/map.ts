650c090c58f258a743d3dcf540bbca4be8bc07e2
Cabe Branson <1258606+bitmap@users.noreply.github.com>
2021-01-27 19:46:55 -0500

    Convert map to TypeScript

---
+++ lib/map.ts
@@ -0,0 +1,23 @@
+import { curry } from './curry'
+
+type Mapper<T, U> = (fn: (value: T) => U, list: T[]) => U[];
+
+const mapper: Mapper<any, any> = (fn, list) => {
+  const { length } = list
+  const result = Array(length)
+  let index = 0
+
+  while (index < length) {
+    result[index] = fn(list[index])
+    index += 1
+  }
+  return result
+}
+
+/**
+ * Apply function to each items in a list, and return a new list. `map` args are
+ * curried.
+ *
+ * `map :: (a -> b) -> [a] -> [b]`
+ */
+export const map = curry(mapper)
