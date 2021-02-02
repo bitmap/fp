c6ae738804609cabfb2a5c60e349b102f7339cd3
Cabe Branson <1258606+bitmap@users.noreply.github.com>
2021-02-02 10:58:31 -0500

    Convert slice to TypeScript

---
+++ lib/slice.ts
@@ -0,0 +1,10 @@
+import { curry } from './curry'
+
+/**
+ * Return a sliced list. `slice` args are curried.
+ *
+ * slice :: number -> number -> [a] -> [a]
+ */
+export const slice = curry(
+  <T>(start: number, end: number, list: T[]): T[] => list.slice(start, end),
+)
