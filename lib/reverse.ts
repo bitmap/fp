f82511300f2d7ea2525339f70674d90fd2921295
Cabe Branson <1258606+bitmap@users.noreply.github.com>
2021-02-02 10:58:18 -0500

    Convert reverse to TypeScript

---
+++ lib/reverse.ts
@@ -0,0 +1,20 @@
+import { isString } from './isString'
+import { join } from './join'
+import { split } from './split'
+
+/**
+ * Reverse a string or items in a list. Unlike `Array.prototype.reverse`, doesn't mutate target.
+ *
+ * reverse :: string -> string
+ * reverse :: [a] -> [a]
+ */
+export const reverse = (data: string | any[]): string | any[] => {
+  const { reverse } = Array.prototype
+  const delimiter = ''
+
+  if (isString(data)) {
+    return join(delimiter, reverse.call(split(delimiter, data as string)))
+  }
+
+  return reverse.call([...data])
+}
