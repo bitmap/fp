f95af1a00cc5cd2a613604c8ab075a052c41af0b
Cabe Branson <1258606+bitmap@users.noreply.github.com>
2021-02-02 11:06:10 -0500

    Convert append, prepend to TypeScript

---
+++ lib/append.ts
@@ -0,0 +1,15 @@
+import { copy } from './copy'
+import { curry } from './curry'
+
+/**
+ * Appends an item to the end of a list. `append` args are curried.
+ *
+ * `append :: a -> [b] -> [b, a]`
+ */
+export const append = curry(
+  <T>(item: T, list: T[]): T[] => {
+    const newList = copy(list)
+    newList.push(item)
+    return newList
+  },
+)
