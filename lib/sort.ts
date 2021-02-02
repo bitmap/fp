8d8a8e9c246030922cdb5bd9b8d147d5b31ccea3
Cabe Branson <1258606+bitmap@users.noreply.github.com>
2021-02-02 11:15:32 -0500

    Convert sort to TypeScript

---
+++ lib/sort.ts
@@ -0,0 +1,11 @@
+import { curry } from './curry'
+
+/**
+ * Sorts items in a list according to comparator function. Unlike
+ * `Array.prototype.sort`, doesn't mutate target. `sort` args are curried.
+ *
+ * `sort :: ((a, a) -> number) -> [a] -> [a]`
+ */
+export const sort = curry(
+  <T>(compareFn: (a: any, b: any) => number, list: T[]): T[] => [...list].sort(compareFn),
+)
