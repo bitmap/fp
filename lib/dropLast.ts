fcadecc28765fda8ebd799937ee4aec698d5cd75
Cabe Branson <1258606+bitmap@users.noreply.github.com>
2021-02-02 10:22:59 -0500

    Update documentation

--- lib/dropLast.ts
+++ lib/dropLast.ts
@@ -1,1 +1,6 @@
+/**
+ * Drops the last item in a list. `dropLast` args are curried.
+ *
+ * dropLast :: number -> [a] -> [a]
+ */
 export const dropLast = <T>(list: T[]): T[] => list.slice(0, list.length - 1)
--- lib/dropRight.ts
+++ lib/dropRight.ts
@@ -1,5 +1,10 @@
 import { curry } from './curry'
 
+/**
+ * Drops `n` items from the end of a list. `dropRight` args are curried.
+ *
+ * dropRight :: number -> [a] -> [a]
+ */
 export const dropRight = curry(
   <T>(n: number, list: T[]): T[] => list.slice(0, -n),
 )
