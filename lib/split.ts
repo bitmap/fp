1de5a73e71072592260cd641a60420070b499df7
Cabe Branson <1258606+bitmap@users.noreply.github.com>
2021-02-02 10:31:39 -0500

    Convert split to TypeScript

---
+++ lib/split.ts
@@ -0,0 +1,10 @@
+import { curry } from './curry'
+
+/**
+ * Splits a string by delimiter into a list. `split` args are curried.
+ *
+ * split :: string -> string -> [string]
+ */
+export const split = curry(
+  (delimiter: string, string: string): string[] => string.split(delimiter),
+)
