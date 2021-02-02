0315ed7f1ef69b72cc6ca513756d59282bd4ffb4
Cabe Branson <1258606+bitmap@users.noreply.github.com>
2021-02-02 17:02:32 -0500

    Convert pluck to TypeScript

---
+++ lib/pluck.ts
@@ -0,0 +1,13 @@
+import { curry } from './curry'
+import { map } from './map'
+import { prop } from './prop'
+
+/**
+ * Returns map of `key` values from a list of objects. `pluck` args are curried.
+ *
+ * `pluck :: k -> {k:v} -> [v]`
+ */
+export const pluck = curry(
+  <P extends string, V>(key: P, list: { [p in P]: V }[]) =>
+    map(item => prop(key, item), list),
+)
