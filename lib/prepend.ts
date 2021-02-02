271184ef7826a79e59cd81b283819ede5e2a30ea
Cabe Branson <1258606+bitmap@users.noreply.github.com>
2021-02-02 11:06:10 -0500

    Convert append, prepend to TypeScript

--- lib/prepend.ts
+++ lib/prepend.ts
@@ -1,6 +1,11 @@
 import { copy } from './copy'
 import { curry } from './curry'
 
+/**
+ * Prepends an item to the beginning of a list. `prepend` args are curried.
+ *
+ * `prepend :: a -> [b] -> [a, b]`
+ */
 export const prepend = curry((item, list) => {
   const newList = copy(list)
   newList.unshift(item)
