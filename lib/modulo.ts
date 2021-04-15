510bde137587c71756b5892e81e1098ac8ac86ac
Cabe Branson <1258606+bitmap@users.noreply.github.com>
2021-04-15 14:30:10 -0400

    Add modulo, remainder

---
+++ lib/modulo.ts
@@ -0,0 +1,11 @@
+import { curry } from './curry'
+
+/**
+ * Returns modulo of two operands. Note that this is different than the
+ * remainder (`%`) operator is JavaScript, and behaves like the mathmatical
+ * definition of modulo or the `%` operator in Python. `modulo` args are
+ * curried.
+ *
+ * `modulo :: number -> number -> number
+ */
+export const modulo = curry((a: number, n: number): number => ((a % n) + n) % n)
