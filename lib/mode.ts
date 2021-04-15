1536e34b4d3381ce694581f675faf15876d69249
Cabe Branson <1258606+bitmap@users.noreply.github.com>
2021-04-15 14:13:06 -0400

    Add mode

---
+++ lib/mode.ts
@@ -0,0 +1,23 @@
+/**
+ * Returns mode (most frequent occurance) of all arguments.
+ *
+ * `mode :: [number] -> number
+ */
+export const mode = (...args: number[]): number => {
+  const dict: Record<string, number> = {}
+  let comparator = 0
+  let mode = null
+
+  for (const item of args) {
+    const freq = dict[item] ? dict[item] + 1 : 1
+
+    if (freq > comparator) {
+      comparator = freq
+      mode = item
+    }
+
+    dict[item] = freq
+  }
+
+  return mode
+}
