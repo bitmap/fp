7a55602d550868ca111ed697676333da3633222b
Cabe Branson <1258606+bitmap@users.noreply.github.com>
2021-04-14 21:39:34 -0400

    Add clamp

---
+++ lib/clamp.ts
@@ -0,0 +1,10 @@
+import { curry } from './curry'
+
+/**
+ * Clips value in mix/max range. `clamp` args are curried.
+ *
+ * `clamp :: number -> number -> number -> number`
+ */
+export const clamp = curry(
+  (min:number, max:number, value:number): number => Math.min(Math.max(value, min), max),
+)
