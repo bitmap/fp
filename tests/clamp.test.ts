a30439909e0ca781404ac26377c1b69d89311c80
Cabe Branson <1258606+bitmap@users.noreply.github.com>
2021-04-14 21:39:34 -0400

    Add clamp

---
+++ tests/clamp.test.ts
@@ -0,0 +1,13 @@
+import { clamp } from '../lib'
+
+describe('clamp', () => {
+  test('clamps value', () => {
+    expect(clamp(0, 1.5, 2)).toEqual(1.5)
+  })
+
+  test('clamps partial', () => {
+    const clamp2 = clamp(-2, 2)
+    expect(clamp2(10)).toEqual(2)
+    expect(clamp2(-5)).toEqual(-2)
+  })
+})
