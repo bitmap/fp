22799a265a728ee5ecca34a7bbe588011066a9af
Cabe Branson <1258606+bitmap@users.noreply.github.com>
2022-01-03 15:52:26 -0500

    Add `groupBy`

---
+++ tests/groupBy.test.ts
@@ -0,0 +1,71 @@
+import { groupBy } from "../lib";
+
+describe("groupBy", () => {
+  const foods = [
+    {
+      category: "fruits",
+      value: "apple",
+    },
+    {
+      category: "fruits",
+      value: "banana",
+    },
+    {
+      category: "vegetable",
+      value: "spinach",
+    },
+    {
+      category: "fruits",
+      value: "orange",
+    },
+    {
+      category: "vegetable",
+      value: "broccoli",
+    },
+  ];
+
+  const groupByCategory = groupBy((item) => item.category);
+
+  test("groups by category", () => {
+    expect(groupByCategory(foods)).toEqual({
+      fruits: [
+        { category: "fruits", value: "apple" },
+        { category: "fruits", value: "banana" },
+        { category: "fruits", value: "orange" },
+      ],
+      vegetable: [
+        { category: "vegetable", value: "spinach" },
+        { category: "vegetable", value: "broccoli" },
+      ],
+    });
+  });
+
+  test("advanced grouping", () => {
+    const byGrade = groupBy((student) => {
+      const { score } = student;
+      return score < 65
+        ? "F"
+        : score < 70
+        ? "D"
+        : score < 80
+        ? "C"
+        : score < 90
+        ? "B"
+        : "A";
+    });
+    const students = [
+      { name: "Cabe", score: 99 },
+      { name: "Saki", score: 100 },
+      { name: "Abby", score: 84 },
+      { name: "Eddy", score: 58 },
+      { name: "Jake", score: 69 },
+    ];
+
+    expect(byGrade(students)).toEqual({
+      A: [{ name: "Cabe", score: 99 }, { name: "Saki", score: 100 }],
+      B: [{ name: "Abby", score: 84 }],
+      D: [{ name: "Jake", score: 69 }],
+      F: [{ name: "Eddy", score: 58 }],
+    });
+  });
+});
