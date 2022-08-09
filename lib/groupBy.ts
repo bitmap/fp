d49eb77f64cb641f6264cb8a178c1eafa7491956
Cabe Branson <1258606+bitmap@users.noreply.github.com>
2022-08-09 09:32:51 -0400

    Improve groupBy typings

--- lib/groupBy.ts
+++ lib/groupBy.ts
@@ -1,15 +1,18 @@
 import { curry } from "./curry";
 import { reduce } from "./reduce";
 
-export const groupBy = curry(<T extends Record<any, any>, K extends keyof any>(
-  getKey: (item: T) => K,
-  list: T[],
-): Record<K, T[]> => reduce(
-    (acc, item) => {
+/**
+ * Returns an object with keys being the result of the given function and values
+ * being arrays of the items in the original array that produced the key.
+ *
+ * groupBy :: (a -> string) -> [a] -> { [string]: [a] }
+ */
+export const groupBy = curry(
+  <T extends Record<string, any>>(getKey: (item: T) => keyof T) =>
+    reduce((acc, item) => {
       const key = getKey(item);
-      (acc[key] = acc[key] || []).push(item);
+      acc[key] = acc[key] || [];
+      acc[key].push(item);
       return acc;
-    },
-    {} as Record<K, T[]>,
-    list,
-  ));
+    }, Object.create(null)),
+);
