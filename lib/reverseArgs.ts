8afd7b2e7b124bc9167d652066856c0f17fafc36
Cabe Branson <1258606+bitmap@users.noreply.github.com>
2026-02-11 13:40:30 -0500

    Improve reverseArgs with proper generic typing

--- lib/reverseArgs.ts
+++ lib/reverseArgs.ts
@@ -1,7 +1,12 @@
-type Fn = (...arg: any[]) => any;
-
-export const reverseArgs =
-  (fn: Fn) =>
-  (...args: any[]): Fn =>
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-    fn(...args.reverse());
+/**
+ * Returns a function that calls the original function with reversed argument order.
+ *
+ * `reverseArgs :: ((...a) -> b) -> ((...a) -> b)`
+ */
+export function reverseArgs<Args extends unknown[], Return>(
+  fn: (...args: Args) => Return,
+): (...args: Args) => Return {
+  return (...args: Args): Return => {
+    return fn(...(args.reverse() as Args));
+  };
+}
