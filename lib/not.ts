cbf02a2ac84fa788a38b5cdb5f058fa36a2fee7b
Cabe Branson <1258606+bitmap@users.noreply.github.com>
2026-02-11 13:49:15 -0500

    Add JSDoc comment to not function

--- lib/not.ts
+++ lib/not.ts
@@ -1,3 +1,8 @@
+/**
+ * Returns a negated version of a predicate function.
+ *
+ * `not :: (a -> boolean) -> (a -> boolean)`
+ */
 export function not<T>(predicate: (...args: T[]) => boolean) {
   return (...args: T[]): boolean => !predicate(...args);
 }
