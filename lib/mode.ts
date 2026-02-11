1c3a5b9d714dded0687fb196d8c463280781b3ce
Cabe Branson <1258606+bitmap@users.noreply.github.com>
2026-02-11 13:10:19 -0500

    Fix mode function to throw error when called with no arguments

--- lib/mode.ts
+++ lib/mode.ts
@@ -4,9 +4,13 @@
  * `mode :: [number] -> number
  */
 export const mode = (...args: number[]): number => {
+  if (args.length === 0) {
+    throw new Error("mode requires at least one argument");
+  }
+
   const dict: Record<string, number> = {};
   let comparator = 0;
-  let mode = null;
+  let mode = args[0];
 
   for (const item of args) {
     const freq = dict[item] ? dict[item] + 1 : 1;
